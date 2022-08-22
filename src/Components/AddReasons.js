db8315e4eec87120e9f5856b5a49dc11de353cef
SardarShubham <sardarshubham17@gmail.com>
2022-08-22 08:26:49 +0530

    add reason modal

--- src/Components/AddReasons.js
+++ src/Components/AddReasons.js
@@ -1,7 +1,36 @@
-import React from "react";
-import { Modal, ModalBody, Button } from "reactstrap";
+import React, { useState } from "react";
+import { Modal, ModalBody, Button, FormGroup, Label, Input } from "reactstrap";
 
 const AddReasons = ({addreasonsModal, toggleAddreason}) => {
+  const [reasonArr, setreasonArr] = useState([]);
+  const arr = [
+    "Lack of Infrastructure",
+    "Lack of hands on learning experience",
+    "Lack of in-demand skills",
+    "Lack of job opportunities",
+    "Lack of training & placement resources",
+    "Different career interest apart from technical education"
+  ];
+
+  const handleOnchange = (e,reason) => {
+    if (e.target.checked){
+      setreasonArr(curr=>[...curr, reason])
+    }
+    else{
+      let index = reasonArr.indexOf(reason);
+      let temp = reasonArr;
+      temp.splice(index,1);
+      console.log(temp);
+      setreasonArr(temp);
+    }
+  }
+
+
+  const handleSubmit = () => {
+    console.log(reasonArr); // reason array of string
+    let userid = localStorage.getItem('userid');  // college id
+    // "https://optimizers-sih-backend.herokuapp.com/api/v1/college/PutReasons"
+  }
   return (
     <Modal isOpen={addreasonsModal} toggle={toggleAddreason}>
       <div className="modal-header justify-content-center">
@@ -10,10 +39,42 @@ const AddReasons = ({addreasonsModal, toggleAddreason}) => {
         </button>
         <h4 className="title title-up">Add reasons of Unemployability</h4>
       </div>
-      <ModalBody></ModalBody>
+      <ModalBody>
+              <p className="category">Select reasons</p>
+
+              {
+                arr.map(reason => {
+                  return(
+                    <FormGroup check>
+                      <Label check>
+                        <Input 
+                          type="checkbox"
+                          onChange={e=>handleOnchange(e,reason)}
+                          // onChange={()=>setreasonArr(curr=>[...curr, reason])}
+                          />
+                        <span className="form-check-sign"></span>
+                        {reason}
+                      </Label>
+                    </FormGroup>
+                  );
+                })
+              }
+              <br/>
+                <FormGroup>
+                  <Label> If Other than above reasons, please specify</Label>
+                  <Input
+                    defaultValue=""
+                    placeholder="Enter reasons here"
+                    type="textbox"          
+                    onBlur={(e)=>(e.target.value !== "" && setreasonArr(curr=>[...curr, e.target.value]))}
+                  ></Input>
+                </FormGroup>
+      </ModalBody>
 
       <div className="modal-footer">
-        <Button className="btn btn-success image-btn" type="button">
+        <Button className="btn btn-success image-btn" type="button"
+          onClick={handleSubmit}
+        >
           Submit
         </Button>
         <Button
